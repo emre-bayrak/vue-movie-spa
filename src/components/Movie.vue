f4c19e60920cc6952605e27c3530768a5293dbf1
Emre <bayrakemre89@gmail.com>
2020-04-23 01:32:04 +0100

    Components added | methods added

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,4 +1,5 @@
 <script>
+import Poster from './Poster.vue';
 export default {
   name: 'Movie',
   props: {
@@ -7,10 +8,10 @@ export default {
       required: true,
     }
   },
+  components: {
+    Poster,
+  },
   computed: {
-    posterPath() {
-      return `https://image.tmdb.org/t/p/w500/${this.movie.poster_path}`
-    },
     movieDetailsPath() {
       return `/movie/${this.movie.id}`;
     },
@@ -21,24 +22,21 @@ export default {
 <template>
   <div class="card">
     <router-link :to="movieDetailsPath">
-      <img class="card-img-top" :src="posterPath" :alt="movie.original_title" />
+      <poster class="card-img-top" :posterName="movie.poster_path" :alt="movie.original_title"/>
     </router-link>
     <div class="card-block">
       <h5 class="card-title">{{ movie.original_title }}</h5>
       <p class="card-text">{{ movie.overview }}</p>
     </div>
     <div class="card-footer">
-      <small class="text-muted">Average rating: {{ movie.vote_average.toFixed(2) }}</small>
+      <small class="text-muted">Average rating: {{ movie.vote_average.toFixed(1) }}</small>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style>
   .card {
     min-width: 250px;
     margin-bottom: 30px;
-  }
-  .card img {
-    width: 100%;
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
+import MovieDetails from '@/components/MovieDetails';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    { path: '/', name: 'Home', component: Home },
+    { path: '/', component: Home },
+    { path: '/movie/:id', component: MovieDetails },
   ],
   mode: 'history',
 });
